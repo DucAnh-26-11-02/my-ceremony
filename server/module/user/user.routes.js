727ad5c133b4b7e128f7d4735e9cf864553a11a8
ADMIN\Admin <ducanhmc24@gmail.com>
2025-04-11 08:44:07 +0700

    log

--- server/module/user/user.routes.js
+++ server/module/user/user.routes.js
@@ -11,7 +11,15 @@ const Configs = require("../../config");
 
 const UserRouter = require("express").Router();
 
-UserRouter.get("/qr/data", getDataQrController);
+UserRouter.get(
+    "/qr/data",
+    (req, res, next) => {
+        const { alias, code } = req;
+        console.log(`${alias || ""}:${code || ""} is getting data!`);
+        next();
+    },
+    getDataQrController
+);
 UserRouter.get("/qr", AppAdminMiddleware(Configs.code), getQrController);
 UserRouter.get("/", AppAdminMiddleware(Configs.code), listController);
 
